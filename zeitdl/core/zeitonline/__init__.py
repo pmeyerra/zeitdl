78a1ef81f782fb0786aa112f765001021a626257
pmeyerra <pmeyerra@gmail.com>
2025-03-15 20:27:44 +0100

    ci: use `ruff` as linter and formatter

--- zeitdl/core/zeitonline/__init__.py
+++ zeitdl/core/zeitonline/__init__.py
@@ -2,4 +2,4 @@ from . import constants
 from .epaper import get_download_url, get_issue_page_url
 from .login import login
 
-__all__ = ["login", "get_issue_page_url", "get_download_url", "constants"]
+__all__ = ["constants", "get_download_url", "get_issue_page_url", "login"]
--- zeitdl/service/__init__.py
+++ zeitdl/service/__init__.py
@@ -1,4 +1,4 @@
 from .credentials import load_credentials_from_file
 from .download import download_file
 
-__all__ = ["load_credentials_from_file", "download_file"]
+__all__ = ["download_file", "load_credentials_from_file"]
